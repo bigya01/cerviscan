3c6987ccfad7f806f92caf8cc49b87290d08d423
sareeka61 <ghimiresarika61@gmail.com>
2024-05-12 01:55:03 +0545

    addition of result component in frontend

---
+++ frontend/cerviscan-frontend/src/components/Result.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import '../css files/result.css'
+import axios from 'axios';
+
+export const Result = () => {
+  const [resultImage, setResultImage] = useState(null);
+
+  const fetchResultImage = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/result-image'); // Change the endpoint accordingly
+      setResultImage(response.data.imageUrl);
+    } catch (error) {
+      console.error('Error fetching result image:', error);
+    }
+  };
+
+  // Call fetchResultImage when the component mounts
+  useEffect(() => {
+    fetchResultImage();
+  }, []);
+
+  return (
+    <>
+      <div className="image-container">
+        {resultImage && (
+          <img src={resultImage} alt="Result" className="image-preview" />
+        )}
+      </div>
+
+      <div className="cervixtypeinfo">
+        
+      </div>
+    </>
+  );
+};
